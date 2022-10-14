a94d1790f91e331702895b1df9308cd392943b58
colbyfayock <fayock@gmail.com>
2022-10-13 22:20:05 -0400

    Step 2: Installing and configuring the Clerk Next.js SDK

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
 import '@styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
 
 export default MyApp
